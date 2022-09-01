334670ef28d14401bbb1b54a014d470d6949fa21
WallSid <103652593+WallSid@users.noreply.github.com>
2022-09-01 15:06:16 +0300

    done section deals

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ const swiper = new Swiper('.main__slider-block', {
       prevEl: '.main__text-block__arrow.swiper-button-prev',
     },
   });
+
+
+  // Таби 
+const tabNavItems = document.querySelectorAll(".best-deals__tabs-button");
+const tabItems = document.querySelectorAll(".item-tabs");
+
+document.addEventListener("click", function(e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+  if (targetElement.closest(".best-deals__tabs-button")) {
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains("_active")) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove("_active");
+      }
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+      
+    });
+    targetElement.classList.add("_active");
+    tabItems[currentActiveIndex].classList.remove("_active");
+    tabItems[newActiveIndex].classList.add("_active");
+  }
+});
